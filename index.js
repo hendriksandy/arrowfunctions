332ac4ecc9bd541b34f7c8307d724658128df146
Andy Hendriks <nikje1@hotmail.com>
2021-05-15 01:10:18 +0200

    arrowfunctions

---
+++ index.js
@@ -0,0 +1,28 @@
+// zet de functie om naar een arrow functie
+//const ikRockArrowFunctions = function () {
+// console.log("Joe, ik rock de arrow functions!");
+// };
+
+//ikRockArrowFunctions()
+
+const ikRockarowFunctions = () => console.log("Joe, ik rock de arrow functions!")
+ikRockarowFunctions();
+
+
+
+
+// const fivePlusSeven = function () {
+//   return 5 + 7
+// };
+
+
+const fivePlusSeven = () => 5 + 7;
+
+fivePlusSeven();
+
+const myFunction = (a, b) => a + b;
+
+const addFive = a => a + 5;
+
+const createObject = () => console.log("hoi");
+createObject();
